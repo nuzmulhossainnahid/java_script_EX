04f6dcd463088ecf2bf71882dd99e8b04c5f8d27
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-10-25 01:02:21 +0600

    vv

--- java_scripts.js
+++ java_scripts.js
@@ -1,1 +1,21 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript!"
+// document.getElementById("demo").innerHTML = "Hello JavaScript!"
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed.";
+//   }
+function nahid(){
+    document.getElementById("h1").innerHTML="I am Nazmyul Hossain Nahid";
+    document.getElementById("h1").style.color="green";
+}
+function nahid1(){
+    document.getElementById("h1").innerHTML="I am Nahid";
+    document.getElementById("h1").style.color="red";
+    window.alert('Hellow');
+    // document.write("lorem100")
+   
+}
+
+let a, b, c;
+a=10;
+b=5;
+c=a+b;
+console.log(c);
