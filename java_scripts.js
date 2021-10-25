dcc8aacb037b280dc6567fa9094ec31bbb118589
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-10-25 10:55:21 +0600

    function

--- java_scripts.js
+++ java_scripts.js
@@ -2,20 +2,28 @@
 // function myFunction() {
 //     document.getElementById("demo").innerHTML = "Paragraph changed.";
 //   }
-function nahid(){
-    document.getElementById("h1").innerHTML="I am Nazmyul Hossain Nahid";
-    document.getElementById("h1").style.color="green";
-}
-function nahid1(){
-    document.getElementById("h1").innerHTML="I am Nahid";
-    document.getElementById("h1").style.color="red";
-    window.alert('Hellow');
-    // document.write("lorem100")
+// function nahid(){
+//     document.getElementById("h1").innerHTML="I am Nazmyul Hossain Nahid";
+//     document.getElementById("h1").style.color="green";
+// }
+// function nahid1(){
+//     document.getElementById("h1").innerHTML="I am Nahid";
+//     document.getElementById("h1").style.color="red";
+//     window.alert('Hellow');
+//     // document.write("lorem100")
    
-}
+// }
 
-let a, b, c;
-a=10;
-b=5;
-c=a+b;
-console.log(c);
+// let a, b, c;
+// a=10;
+// b=5;
+// c=a+b;
+// console.log(c);
+
+function toCelsius(x) {
+    return (5/9) * (x -32);
+  }
+function button(){
+   var x= document.getElementById("input").value;
+   document.getElementById("h1").innerHTML = toCelsius(x);
+}
